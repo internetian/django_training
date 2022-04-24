8d4e87627cba0a83b889cb53f7802df7cfbccbdf
internetian <internetian@hotmail.com>
2022-04-25 02:59:30 +0900

    123page

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from blog import views
 
 app_name='blog'
 urlpatterns=[
-    path('',PostLV.as_view(), name='index'),
+    path('',views.PostLV.as_view(), name='index'),
     path('post/', views.PostLV.as_view(), name='post_list'),
     re_path(r'^post/(?P<slug>[-\w]+)/$',views.PostDV.as_view(), name='post_detail'),
-    path('archieve/',views.PostAV.as_view(), name='psot_archieve'),
-    path('archieve/<int:year>/',views.PostYAV.as_view(), name='post_year_archieve'),
-    path('archieve/<int:year>/<str:month>/',views.PostMAV.as_view(), name='post_month_archieve'),
-    path('archieve/<int:year>/<str:month>/<int:day>/',views.PostDAV.as_view(), name='post_day_archieve'),
-    path('archieve/today/',views.PostTAV.as_view(), name='post_today_archieve'),
+    path('archive/',views.PostAV.as_view(), name='post_archive'),
+    path('archive/<int:year>/',views.PostYAV.as_view(), name='post_year_archive'),
+    path('archive/<int:year>/<str:month>/',views.PostMAV.as_view(), name='post_month_archive'),
+    path('archive/<int:year>/<str:month>/<int:day>/',views.PostDAV.as_view(), name='post_day_archive'),
+    path('archive/today/',views.PostTAV.as_view(), name='post_today_archive'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from blog.models import Post
 class PostLV(ListView):
     model=Post
     template_name='blog/post_all.html'
-    ccontext_object_name='posts'
+    context_object_name='posts'
     paginate_by=2
 
 class PostDV(DetailView):
